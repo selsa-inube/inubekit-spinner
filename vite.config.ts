2ef33f9c70653360d103be9ddea6879a727e5c9e
Andres Babativa <carlos3k11@gmail.com>
2024-02-05 15:03:29 -0500

    feat: implement the distribution of the type definition

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,5 +20,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [react(), dts({ rollupTypes: true })],
 });
