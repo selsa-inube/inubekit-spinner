59fb2305b45c601c60ec775bfdc49ef7a81ab7c9
cmarin001 <cmarin6051@gmail.com>
2024-09-27 11:09:56 -0500

    chore: move spinner tokens from foundations to component

---
+++ src/Spinner/Tokens/tokens.ts
@@ -0,0 +1,150 @@
+import { inube } from "@inubekit/foundations";
+
+const tokens = {
+  primary: {
+    solid: {
+      spin: {
+        color: inube.palette.blue.B400,
+      },
+      track: {
+        color: inube.palette.neutral.N30,
+      },
+    },
+    transparent: {
+      spin: {
+        color: inube.palette.blue.B400,
+      },
+      track: {
+        color: inube.palette.neutralAlpha.N0A,
+      },
+    },
+  },
+  success: {
+    solid: {
+      spin: {
+        color: inube.palette.green.G400,
+      },
+      track: {
+        color: inube.palette.neutral.N30,
+      },
+    },
+    transparent: {
+      spin: {
+        color: inube.palette.green.G400,
+      },
+      track: {
+        color: inube.palette.neutralAlpha.N0A,
+      },
+    },
+  },
+  warning: {
+    solid: {
+      spin: {
+        color: inube.palette.yellow.Y400,
+      },
+      track: {
+        color: inube.palette.neutral.N30,
+      },
+    },
+    transparent: {
+      spin: {
+        color: inube.palette.yellow.Y400,
+      },
+      track: {
+        color: inube.palette.neutralAlpha.N0A,
+      },
+    },
+  },
+  danger: {
+    solid: {
+      spin: {
+        color: inube.palette.red.R400,
+      },
+      track: {
+        color: inube.palette.neutral.N30,
+      },
+    },
+    transparent: {
+      spin: {
+        color: inube.palette.red.R400,
+      },
+      track: {
+        color: inube.palette.neutralAlpha.N0A,
+      },
+    },
+  },
+  help: {
+    solid: {
+      spin: {
+        color: inube.palette.purple.P400,
+      },
+      track: {
+        color: inube.palette.neutral.N30,
+      },
+    },
+    transparent: {
+      spin: {
+        color: inube.palette.purple.P400,
+      },
+      track: {
+        color: inube.palette.neutralAlpha.N0A,
+      },
+    },
+  },
+  dark: {
+    solid: {
+      spin: {
+        color: inube.palette.neutral.N900,
+      },
+      track: {
+        color: inube.palette.neutral.N30,
+      },
+    },
+    transparent: {
+      spin: {
+        color: inube.palette.neutral.N900,
+      },
+      track: {
+        color: inube.palette.neutralAlpha.N0A,
+      },
+    },
+  },
+  gray: {
+    solid: {
+      spin: {
+        color: inube.palette.neutral.N100,
+      },
+      track: {
+        color: inube.palette.neutral.N30,
+      },
+    },
+    transparent: {
+      spin: {
+        color: inube.palette.neutral.N100,
+      },
+      track: {
+        color: inube.palette.neutralAlpha.N0A,
+      },
+    },
+  },
+  light: {
+    solid: {
+      spin: {
+        color: inube.palette.neutral.N10,
+      },
+      track: {
+        color: inube.palette.neutral.N30,
+      },
+    },
+    transparent: {
+      spin: {
+        color: inube.palette.neutral.N10,
+      },
+      track: {
+        color: inube.palette.neutralAlpha.N0A,
+      },
+    },
+  },
+};
+
+export { tokens };
