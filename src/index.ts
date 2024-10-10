a46b762b0f3f8a32605ccc499f5128268bda950d
Cesar Marin Alfonso <45011420+cmarin001@users.noreply.github.com>
2024-10-10 12:07:10 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Spinner } from "./Spinner";
 export type { ISpinner } from "./Spinner";
 export type { ISpinnerSize, ISpinnerAppearance } from "./Spinner/props";
+export { tokens as SpinnerTokens } from "./Spinner/Tokens/tokens";
